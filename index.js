f4f3a442cee1ac886f57ff053a50e96ee5be6500
hmoon630 <s19017@gsm.hs.kr>
2019-07-18 19:52:18 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = "Hello Koa";
+});
+
+app.listen(3000, ()=>{
+    console.log('moon\'s server is listening to port 3000');
+});
